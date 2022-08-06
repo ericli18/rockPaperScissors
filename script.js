76304f237b03131cab52bbbc9b17222fe334771c
Eric Li <eric.li.8736@gmail.com>
2022-08-06 16:06:26 -0500

    Add getComputerChoice funtion

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3);
+    if(!num) return "rock";
+    else
+    return num%2? "paper" : "scissors";
+}
+
+function playRound(playerChoice, computerChoice = "getComputerChoice()") {
+    playerChoice = playerChoice.toLowerCase();
+    if(playerChoice != "rock" && playerChoice != "scissors" && playerChoice != "paper")
+        return "Please enter valid input";
+    if(playerChoice === "rock"){
+
+    }
+    else if(playerChoice === "paper"){
+
+    }
+    else{
+        
+    }
+}
+
+console.log(getComputerChoice());
